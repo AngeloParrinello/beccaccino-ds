2a3d3223ecbbcc8a4b9529ff984490856a16c571
Angelo Parrinello <angelo.parrinello@studio.unibo.it>
2022-02-23 17:38:59 +0100

    Implements pattern Builder for queue creation

---
+++ server/src/main/java/it/unibo/sd/beccacino/rabbitmq/RabbitMQQueueBuilder.java
@@ -0,0 +1,34 @@
+package it.unibo.sd.beccacino.rabbitmq;
+
+import com.rabbitmq.client.BuiltinExchangeType;
+import com.rabbitmq.client.Channel;
+
+import java.io.IOException;
+import java.util.Map;
+
+public interface RabbitMQQueueBuilder {
+
+    RabbitMQQueueBuilder getInstanceOfQueueBuilder();
+
+    RabbitMQQueueBuilder setNameQueue(String nameQueue);
+
+    RabbitMQQueueBuilder setChannel(Channel channel);
+
+    RabbitMQQueueBuilder setDurable(boolean durable);
+
+    RabbitMQQueueBuilder setExclusive(boolean exclusive);
+
+    RabbitMQQueueBuilder setAutoDelete(boolean autoDelete);
+
+    RabbitMQQueueBuilder setRoutingKey(String routingKey);
+
+    RabbitMQQueueBuilder setExchangeType(BuiltinExchangeType exchangeType);
+
+    RabbitMQQueueBuilder setExchangeName(String exchangeName);
+
+    RabbitMQQueueBuilder setArguments(Map<String, Object> arguments);
+
+    String createQueueForReceive() throws IOException;
+
+    String createQueueForSend() throws IOException;
+}
---
+++ server/src/main/java/it/unibo/sd/beccacino/rabbitmq/RabbitMQQueueBuilderImpl.java
@@ -0,0 +1,94 @@
+package it.unibo.sd.beccacino.rabbitmq;
+
+import com.rabbitmq.client.BuiltinExchangeType;
+import com.rabbitmq.client.Channel;
+
+import java.io.IOException;
+import java.util.Map;
+
+public class RabbitMQQueueBuilderImpl implements RabbitMQQueueBuilder{
+
+    private String nameQueue;
+    private Channel channel;
+    private boolean durable = false;
+    private boolean exclusive = false;
+    private boolean autoDelete = true;
+    private Map<String, Object> arguments = null;
+    private String exchangeName = "";
+    private BuiltinExchangeType exchangeType = BuiltinExchangeType.DIRECT;
+    private String routingKey = "";
+
+    @Override
+    public RabbitMQQueueBuilder getInstanceOfQueueBuilder() {
+        return new RabbitMQQueueBuilderImpl();
+    }
+
+    @Override
+    public RabbitMQQueueBuilder setNameQueue(String nameQueue) {
+        this.nameQueue = nameQueue;
+        return this;
+    }
+
+    @Override
+    public RabbitMQQueueBuilder setChannel(Channel channel) {
+        this.channel = channel;
+        return this;
+    }
+
+    @Override
+    public RabbitMQQueueBuilder setDurable(boolean durable){
+        this.durable = durable;
+        return this;
+    }
+
+    @Override
+    public RabbitMQQueueBuilder setExclusive(boolean exclusive){
+        this.exclusive = exclusive;
+        return this;
+    }
+
+    @Override
+    public RabbitMQQueueBuilder setAutoDelete(boolean autoDelete){
+        this.autoDelete = autoDelete;
+        return this;
+    }
+
+    @Override
+    public RabbitMQQueueBuilder setRoutingKey(String routingKey) {
+        this.routingKey = routingKey;
+        return this;
+    }
+
+    @Override
+    public RabbitMQQueueBuilder setExchangeType(BuiltinExchangeType exchangeType) {
+        this.exchangeType = exchangeType;
+        return this;
+    }
+
+    @Override
+    public RabbitMQQueueBuilder setExchangeName(String exchangeName) {
+        this.exchangeName = exchangeName;
+        return this;
+    }
+
+    @Override
+    public RabbitMQQueueBuilder setArguments(Map<String, Object> arguments){
+        this.arguments = arguments;
+        return this;
+    }
+
+    @Override
+    public String createQueueForReceive() throws IOException {
+        this.channel.queueDeclare(this.nameQueue, this.durable, this.exclusive, this.autoDelete, this.arguments);
+        return this.nameQueue;
+    }
+
+    @Override
+    public String createQueueForSend() throws IOException {
+        this.createQueueForReceive();
+        this.channel.exchangeDeclare(this.exchangeName, this.exchangeType);
+        this.channel.queueBind(this.nameQueue, this.exchangeName, this.routingKey);
+        return this.nameQueue;
+    }
+
+}
