64d3e9857fe1a7651561a5ce4f923a9b88af9405
paolo <paolo.penazzi@studio.unibo.it>
2022-02-23 18:18:21 +0100

    Add @BeforeEach method to DBManagerClass to avoid repetition of code

--- server/src/test/java/it/unibo/sd/beccacino/DBManagerTest.java
+++ server/src/test/java/it/unibo/sd/beccacino/DBManagerTest.java
@@ -1,8 +1,8 @@
 package it.unibo.sd.beccacino;
 
 import org.bson.Document;
-import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
@@ -17,30 +17,28 @@ class DBManagerTest {
         this.dbManager.getDB().getCollection("players").drop();
     }
 
+    @BeforeEach
+    void addItems() {
+        this.dbManager.insertDocument(new Document("player_id", "45012"), "players");
+        this.dbManager.insertDocument(new Document("player_id", "333"), "players");
+        this.dbManager.insertDocument(new Document("player_id", "4"), "players");
+    }
+
     @Test void testCreateDBManager() {
         assertEquals("beccacino", this.dbManager.getDB().getName());
     }
 
     @Test void testRemoveDocuments() {
-        this.dbManager.insertDocument(new Document("player_id", "45012"), "players");
-        this.dbManager.insertDocument(new Document("player_id", "333"), "players");
-        this.dbManager.insertDocument(new Document("player_id", "4"), "players");
         this.dbManager.removeDocument("player_id", "333", "players");
         assertEquals(2, dbManager.getDB().getCollection("players").countDocuments());
     }
 
     @Test void testRetrieveAllDocuments() {
-        this.dbManager.insertDocument(new Document("player_id", "45012"), "players");
-        this.dbManager.insertDocument(new Document("player_id", "333"), "players");
-        this.dbManager.insertDocument(new Document("player_id", "4"), "players");
         ArrayList<Document> playersList = this.dbManager.retrieveAllDocuments("players");
         assertEquals(3, playersList.size());
     }
 
     @Test void testInsertDocuments() {
-        this.dbManager.insertDocument(new Document("player_id", "23"), "players");
-        this.dbManager.insertDocument(new Document("player_id", "20"), "players");
-        this.dbManager.insertDocument(new Document("player_id", "4"), "players");
         assertEquals(3, dbManager.getDB().getCollection("players").countDocuments());
     }
 }
