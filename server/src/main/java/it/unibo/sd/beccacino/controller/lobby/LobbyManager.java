1ae4aa9a366c17e69df69966a695c36fe7147028
paolo <paolo.penazzi@studio.unibo.it>
2022-03-08 16:31:08 +0100

    Create method handleRequest

--- server/src/main/java/it/unibo/sd/beccacino/controller/lobby/LobbyManager.java
+++ server/src/main/java/it/unibo/sd/beccacino/controller/lobby/LobbyManager.java
@@ -1,39 +1,11 @@
 package it.unibo.sd.beccacino.controller.lobby;
 
 import it.unibo.sd.beccacino.Lobby;
+import it.unibo.sd.beccacino.Request;
 
 import java.util.List;
 
 public interface LobbyManager {
 
-    /**
-     * Get all the lobbies currently available.
-     * @return the lobbies list.
-     */
-    void getLobbies();
-
-    /**
-     * Create a new lobby.
-     * @return the lobby's ID.
-     */
-    void createLobby();
-
-    /**
-     * Get a specific lobby.
-     * @param lobbyID the lobby's ID.
-     * @return The lobby.
-     */
-    Lobby getLobby(int lobbyID);
-
-    /**
-     * Join an existent lobby.
-     * @param lobbyID the lobby's ID.
-     */
-    void joinLobby(int lobbyID);
-
-    /**
-     *
-     * @param lobbyID
-     */
-    void leaveLobby(int lobbyID);
+    void handleRequest(Request request);
 }
--- server/src/main/java/it/unibo/sd/beccacino/controller/lobby/LobbyManagerImpl.java
+++ server/src/main/java/it/unibo/sd/beccacino/controller/lobby/LobbyManagerImpl.java
@@ -1,30 +1,33 @@
 package it.unibo.sd.beccacino.controller.lobby;
 
 import it.unibo.sd.beccacino.Lobby;
+import it.unibo.sd.beccacino.Player;
+import it.unibo.sd.beccacino.Request;
 import it.unibo.sd.beccacino.controller.lobby.LobbyManager;
 
 public class LobbyManagerImpl implements LobbyManager {
-    @Override
-    public void getLobbies() {}
+    private int id;
+    private String message;
+    private Player sender;
 
     @Override
-    public void createLobby() {
+    public void handleRequest(Request request) {
+        this.id = request.getId();
+        this.message = request.getLobbyMessage();
+        this.sender = request.getRequestingPlayer();
     }
+
+    private void getLobbies() {}
+
+    private void createLobby() {}
 
     private void deleteLobby(int lobbyID) {}
 
-    @Override
-    public Lobby getLobby(int lobbyID) {
+    private Lobby getLobby(int lobbyID) {
         return null;
     }
 
-    @Override
-    public void joinLobby(int lobbyID) {
+    public void joinLobby(int lobbyID) {}
 
-    }
-
-    @Override
-    public void leaveLobby(int lobbyID) {
-
-    }
+    public void leaveLobby(int lobbyID) {}
 }
