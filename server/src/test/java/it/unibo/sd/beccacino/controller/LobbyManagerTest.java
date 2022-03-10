29eebe60633fc8edf37c9f68405ce15d8d46232c
Angelo Parrinello <angelo.parrinello@studio.unibo.it>
2022-03-10 13:32:53 +0100

    Insert new test (TDD) for new features about lobby managing: at the moment we only implement test about lobby creation phase

--- server/src/test/java/it/unibo/sd/beccacino/controller/LobbyManagerTest.java
+++ server/src/test/java/it/unibo/sd/beccacino/controller/LobbyManagerTest.java
@@ -1,5 +1,39 @@
 package it.unibo.sd.beccacino.controller;
 
+import it.unibo.sd.beccacino.DBManager;
+import it.unibo.sd.beccacino.Lobby;
+import it.unibo.sd.beccacino.Player;
+import it.unibo.sd.beccacino.Request;
+import it.unibo.sd.beccacino.controller.lobby.LobbiesStub;
+import it.unibo.sd.beccacino.controller.lobby.LobbyManager;
+import it.unibo.sd.beccacino.controller.lobby.LobbyManagerImpl;
+import org.bson.Document;
+import org.junit.jupiter.api.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class LobbyManagerTest {
+
+    private final LobbiesStub lobbiesStub = new LobbiesStub();
+    private final LobbyManager lobbyManager = new LobbyManagerImpl(lobbiesStub);
+    private Player player;
+
+    @BeforeEach
+    void setUp(){
+        this.player = Player.newBuilder().setId(4).setNickname("Paperino").build();
+    }
+
+    @Test
+    void testCreateLobby() {
+        this.lobbyManager.handleRequest(Request.newBuilder()
+                                               .setLobbyMessage("create")
+                                               .setRequestingPlayer(this.player)
+                                               .build());
+        System.out.println(this.lobbiesStub.getLastOperation());
+    }
 
 }
