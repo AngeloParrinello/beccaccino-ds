bd56a80751474598338ab5849d5b1950ab69c693
paolo <paolo.penazzi@studio.unibo.it>
2022-03-16 17:22:04 +0100

    Add method toInteger and toString to response code enum

--- server/src/main/java/it/unibo/sd/beccacino/ResponseCode.java
+++ server/src/main/java/it/unibo/sd/beccacino/ResponseCode.java
@@ -2,9 +2,26 @@ package it.unibo.sd.beccacino;
 
 public enum ResponseCode {
     OK(200, "Ok"),
-    CREATE(400, "Cannot create lobby"),
-    LEAVE(401, "Cannot leave lobby"),
-    JOIN(402, "Cannot join lobby");
+    CREATE(400, "Cannot create lobby."),
+    LEAVE(401, "Cannot leave lobby."),
+    JOIN(402, "Cannot join lobby."),
+    START(403, "Cannot start the match."),
+    PERMISSION_DENIED(405, "Permission denied.");
 
-    ResponseCode(int code, String message) {}
+    private final int code;
+    private final String message;
+    
+    public int toInteger(ResponseCode responseCode) {
+        return this.code;
+    }
+
+    @Override
+    public String toString() {
+        return this.message;
+    }
+
+    ResponseCode(int code, String message) {
+        this.code = code;
+        this.message = message;
+    }
 }
