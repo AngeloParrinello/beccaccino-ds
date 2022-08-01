137a298b5df17a8b857194b337d47e5a4822bafb
Angelo Parrinello <angelo.parrinello@studio.unibo.it>
2022-08-01 12:03:53 +0200

    Delete settings button

--- client/app/src/main/java/com/example/beccaccino/MainActivity.java
+++ client/app/src/main/java/com/example/beccaccino/MainActivity.java
@@ -1,50 +1,21 @@
 package com.example.beccaccino;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.lifecycle.ViewModelProviders;
-
 import android.app.AlertDialog;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.text.Editable;
 import android.util.Log;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-
-import com.firebase.ui.auth.AuthUI;
-import com.firebase.ui.auth.IdpResponse;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.ActionCodeSettings;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import androidx.appcompat.app.AppCompatActivity;
+import java.io.*;
 import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 
 public class MainActivity extends AppCompatActivity {
-    private static final int RC_SIGN_IN = 111;
-    private static boolean _running = true;
     public static final String PATH_TO_USERNAME = "username_file";
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -68,14 +39,6 @@ public class MainActivity extends AppCompatActivity {
             // pop up per inserire la partita e attesa di risposta dal server dopo aver inserito il codice
             Toast.makeText(MainActivity.this, "Inserire numero prima o poi...", Toast.LENGTH_LONG);
         });
-
-        Button settings = findViewById(R.id.impostazioni);
-        settings.setOnClickListener((v -> {
-            Intent myIntent = new Intent(MainActivity.this, SettingsActivity.class);
-            MainActivity.this.startActivity(myIntent);
-        }));
-
-
     }
         @Override
         protected void onStart() {
@@ -124,14 +87,11 @@ public class MainActivity extends AppCompatActivity {
                 //What ever you want to do with the value
                 Editable usernameChoosed = userName.getText();
 
-
                 String filename = PATH_TO_USERNAME;
                 String fileContents = usernameChoosed.toString();
 
                 try (FileOutputStream fos = context.openFileOutput(filename, Context.MODE_PRIVATE)) {
                     fos.write(fileContents.getBytes());
-                } catch (FileNotFoundException e) {
-                    e.printStackTrace();
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
