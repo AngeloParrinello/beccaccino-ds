7376f5e2d0916ea4fe11daf5ae63c2d23175646d
Angelo Parrinello <angelo.parrinello@studio.unibo.it>
2022-08-01 14:52:06 +0200

    Code clean up

--- client/app/src/main/java/com/example/beccaccino/MainActivity.java
+++ client/app/src/main/java/com/example/beccaccino/MainActivity.java
@@ -8,8 +8,8 @@ import android.text.Editable;
 import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
+
 import java.io.*;
 import java.nio.charset.StandardCharsets;
 
@@ -39,89 +39,100 @@ public class MainActivity extends AppCompatActivity {
             searchMatch(this);
         });
     }
-        @Override
-        protected void onStart() {
-            super.onStart();
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        MusicManager.pause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        /*MUSIC*/
+        if (getSharedPreferences("Settings", MODE_PRIVATE).getBoolean("music", false)) {
+            MusicManager.start(this, 0);
         }
+    }
 
-        private void searchMatch(final Context context) {
-                AlertDialog.Builder alert = new AlertDialog.Builder(context);
-                final EditText matchID = new EditText(context);
-                alert.setMessage("Digita l'ID della partita");
-                alert.setTitle("MatchID");
-                alert.setView(matchID);
-                alert.setCancelable(false);
-                alert.setPositiveButton("Cerca", (dialog, whichButton) -> {
-                    String matchIDInserted = matchID.getText().toString();
+    private static void setUsername(final Context context) {
+        AlertDialog.Builder alert = new AlertDialog.Builder(context);
+        final EditText userName = new EditText(context);
+        alert.setMessage("Scegli il tuo Username");
+        alert.setTitle("Username");
+        alert.setView(userName);
+        alert.setCancelable(false);
+        alert.setPositiveButton("Confirm", (dialog, whichButton) -> {
+            //What ever you want to do with the value
+            Editable usernameChoosed = userName.getText();
+            String fileContents = usernameChoosed.toString();
 
-                    //TODO: chiamare Server con Rabbit
-                });
-                alert.show();
-        }
-
-        private void checkUsername() throws IOException {
-            String[] files = this.fileList();
-            boolean isPresent = false;
-            for(int i=0; i<files.length; i++){
-                if(files[i].equals(PATH_TO_USERNAME)){
-                    isPresent = true;
-                }
-            }
-
-            if(isPresent) {
-                FileInputStream fis = getApplicationContext().openFileInput(PATH_TO_USERNAME);
-                InputStreamReader inputStreamReader =
-                        new InputStreamReader(fis, StandardCharsets.UTF_8);
-                StringBuilder stringBuilder = new StringBuilder();
-                try (BufferedReader reader = new BufferedReader(inputStreamReader)) {
-                    String line = reader.readLine();
-                    while (line != null) {
-                        stringBuilder.append(line).append('\n');
-                        line = reader.readLine();
-                    }
-                } catch (IOException e) {
-                    // Error occurred when opening raw file for reading.
-                } finally {
-                    String username = stringBuilder.toString();
-                    Log.d("MyApp",username);
-                }
-            } else {
-                setUsername(this);
-            }
-        }
-
-        public static void setUsername(final Context context) {
-            AlertDialog.Builder alert = new AlertDialog.Builder(context);
-            final EditText userName = new EditText(context);
-            alert.setMessage("Scegli il tuo Username");
-            alert.setTitle("Username");
-            alert.setView(userName);
-            alert.setCancelable(false);
-            alert.setPositiveButton("Confirm", (dialog, whichButton) -> {
-                //What ever you want to do with the value
-                Editable usernameChoosed = userName.getText();
-
-                String filename = PATH_TO_USERNAME;
-                String fileContents = usernameChoosed.toString();
-
-                try (FileOutputStream fos = context.openFileOutput(filename, Context.MODE_PRIVATE)) {
-                    fos.write(fileContents.getBytes());
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-
-
-            });
-            alert.show();
-        }
-
-        public static String getUsername(Context context){
-            FileInputStream fis = null;
-            try {
-                fis = context.openFileInput(PATH_TO_USERNAME);
-            } catch (FileNotFoundException e) {
+            try (FileOutputStream fos = context.openFileOutput(PATH_TO_USERNAME, Context.MODE_PRIVATE)) {
+                fos.write(fileContents.getBytes());
+            } catch (IOException e) {
                 e.printStackTrace();
             }
+
+        });
+        alert.show();
+    }
+
+    public static String getUsername(Context context) {
+        FileInputStream fis = null;
+        try {
+            fis = context.openFileInput(PATH_TO_USERNAME);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        InputStreamReader inputStreamReader =
+                new InputStreamReader(fis, StandardCharsets.UTF_8);
+        StringBuilder stringBuilder = new StringBuilder();
+        try (BufferedReader reader = new BufferedReader(inputStreamReader)) {
+            String line = reader.readLine();
+            while (line != null) {
+                stringBuilder.append(line).append('\n');
+                line = reader.readLine();
+            }
+        } catch (IOException e) {
+            // Error occurred when opening raw file for reading.
+        }
+        String username = stringBuilder.toString();
+        return username.substring(0, username.length() - 1);
+    }
+
+    private void searchMatch(final Context context) {
+        AlertDialog.Builder alert = new AlertDialog.Builder(context);
+        final EditText matchID = new EditText(context);
+        alert.setMessage("Digita l'ID della partita");
+        alert.setTitle("MatchID");
+        alert.setView(matchID);
+        alert.setCancelable(false);
+        alert.setPositiveButton("Cerca", (dialog, whichButton) -> {
+            String matchIDInserted = matchID.getText().toString();
+
+            //TODO: chiamare Server con Rabbit
+        });
+        alert.show();
+    }
+
+    private void checkUsername() throws IOException {
+        String[] files = this.fileList();
+        boolean isPresent = false;
+
+        for (String file : files) {
+            if (file.equals(PATH_TO_USERNAME)) {
+                isPresent = true;
+                break;
+            }
+        }
+
+        if (isPresent) {
+            FileInputStream fis = getApplicationContext().openFileInput(PATH_TO_USERNAME);
             InputStreamReader inputStreamReader =
                     new InputStreamReader(fis, StandardCharsets.UTF_8);
             StringBuilder stringBuilder = new StringBuilder();
@@ -133,23 +144,14 @@ public class MainActivity extends AppCompatActivity {
                 }
             } catch (IOException e) {
                 // Error occurred when opening raw file for reading.
+            } finally {
+                String username = stringBuilder.toString();
+                Log.d("MyApp", username);
             }
-            String username = stringBuilder.toString();
-            return username.substring(0,username.length()-1);
-        }
-
-    protected void onPause() {
-        super.onPause();
-        MusicManager.pause();
-
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        /*MUSIC*/
-        if(getSharedPreferences("Settings", MODE_PRIVATE).getBoolean("music", false)){
-            MusicManager.start(this,0);
+        } else {
+            setUsername(this);
         }
     }
+
+
 }
