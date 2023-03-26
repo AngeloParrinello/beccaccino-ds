d2f758cc5b6e7a7722b726081475345dfe66367b
Angelo Parrinello <angelo.parrinello@studio.unibo.it>
2023-03-26 16:48:28 +0200

    test: delete useless beforeeach clause and add comments

--- server/src/test/java/it/unibo/sd/beccacino/DBManagerTest.java
+++ server/src/test/java/it/unibo/sd/beccacino/DBManagerTest.java
@@ -1,30 +1,19 @@
 package it.unibo.sd.beccacino;
 
-import com.mongodb.BasicDBObject;
 import org.bson.Document;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
-import static com.mongodb.client.model.Filters.eq;
-import static com.mongodb.client.model.Updates.set;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class DBManagerTest {
-    private DBManager dbManager;
     private static final String TEST_COLLECTION_NAME = "players";
+    private final DBManager dbManager = new DBManager();
     private final Document firstDocument = new Document("_id", "1");
     private final Document secondDocument = new Document("_id", "2");
     private final Document thirdDocument = new Document("_id", "3");
-
-    @BeforeEach
-    void setup() {
-        this.dbManager = new DBManager();
-    }
 
     @AfterEach
     void clearCollection() {
@@ -32,17 +21,29 @@ class DBManagerTest {
         this.dbManager.getDB().getCollection("lobbies").drop();
     }
 
-    @Test void testCreateDBManager() {
+    /**
+     * Test if the system correctly create the database.
+     */
+    @Test
+    void testCreateDBManager() {
         final String databaseName = "beccacino";
         assertEquals(databaseName, this.dbManager.getDB().getName());
     }
 
-    @Test void testInsertDocuments() {
+    /**
+     * Test if the system correctly insert a document in the database.
+     */
+    @Test
+    void testInsertDocuments() {
         this.dbManager.insertDocument(firstDocument, TEST_COLLECTION_NAME);
         assertEquals(1, dbManager.getDB().getCollection(TEST_COLLECTION_NAME).countDocuments());
     }
 
-    @Test void testRetrieveAllDocuments() {
+    /**
+     * Test if the system correctly retrieve all the documents from a specific collection.
+     */
+    @Test
+    void testRetrieveAllDocuments() {
         this.dbManager.insertDocument(firstDocument, TEST_COLLECTION_NAME);
         this.dbManager.insertDocument(secondDocument, TEST_COLLECTION_NAME);
         this.dbManager.insertDocument(thirdDocument, TEST_COLLECTION_NAME);
@@ -50,32 +51,25 @@ class DBManagerTest {
         assertEquals(3, documentRetrieved.size());
     }
 
-    @Test void testRetrieveDocument() {
+    /**
+     * Test if the system correctly retrieve one document from a specific collection.
+     */
+    @Test
+    void testRetrieveDocument() {
         this.dbManager.insertDocument(firstDocument, TEST_COLLECTION_NAME);
         this.dbManager.insertDocument(secondDocument, TEST_COLLECTION_NAME);
         Document testDocument = new Document("_id", "2");
         assertEquals(testDocument, this.dbManager.retrieveDocumentByID("_id", "2", TEST_COLLECTION_NAME));
     }
 
-    @Test void testUpdateDocument() {
+    /**
+     * Test if the system correctly update a document from a specific collection.
+     */
+    @Test
+    void testUpdateDocument() {
         this.dbManager.insertDocument(firstDocument.append("Nickname", "Pippo").append("Gender", "Male"), TEST_COLLECTION_NAME);
         Document testDocument = firstDocument.append("Nickname", "Pluto");
         this.dbManager.updateDocument("1", testDocument, TEST_COLLECTION_NAME);
         assertEquals(testDocument, this.dbManager.retrieveDocumentByID("_id", "1", TEST_COLLECTION_NAME));
     }
-/*
-    @Test void testDeleteDocument() {
-        Document document = new Document("_id", "5");
-        List<BasicDBObject> milestones = new ArrayList<>();
-        milestones.add(new BasicDBObject("milestone_id", "2333"));
-        milestones.add(new BasicDBObject("milestone_id", "2111"));
-        document.put("milestones", milestones);
-        this.dbManager.insertDocument(document, TEST_COLLECTION_NAME);
-
-        this.dbManager.clearMilestones();
-        System.out.println(document);
-        System.out.println(this.dbManager.retrieveDocumentByID("_id", "5", TEST_COLLECTION_NAME));
-        assertNotEquals(document, this.dbManager.retrieveDocumentByID("_id", "5", TEST_COLLECTION_NAME));
-    }
- */
 }
